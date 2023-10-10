907861ebc5797be8f0d2386b24b62fc1015114a1
febriekaa <febryekhawahyuni95@gmail.com>
2023-10-10 13:40:29 +0800

    day 2

---
+++ Day2.java
@@ -0,0 +1,26 @@
+package day.pkg2;
+public class Day2 {
+
+    public static void main(String[] args) {
+      String nama, alamat, fakultas, jurusan;
+      nama = "bika";
+      alamat = "kandeapi";
+      fakultas = "TEKNIK";
+      jurusan = "Informatika";
+      
+      byte umur = 18;
+      int tinggi_badan = 160; 
+      float berat_badan = 55.5f;
+       
+        System.out.println("nama: " + nama);
+        System.out.println("alamat: " +alamat);
+        System.out.println("fakultas: " +fakultas);
+        System.out.println("jurusan: " +jurusan);
+        System.out.println("umur:" +umur);
+        System.out.println("tinggi badan:" +tinggi_badan +"cm");
+        System.out.println("berat badan:" +berat_badan +"kg");
+      
+      
+    }
+    
+}
