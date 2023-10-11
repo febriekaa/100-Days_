477de8ac6acfdf8fa63ec024097895f3ba3746f3
febriekaa <febryekhawahyuni95@gmail.com>
2023-10-11 14:59:27 +0800

    day 3

---
+++ Day3.java
@@ -0,0 +1,21 @@
+
+package day.pkg3;
+public class Day3 {
+
+    public static void main(String[] args) {
+       
+       int angka = 4;
+       int angka2 = 6; 
+       
+      
+         
+        System.out.println(angka2+angka);
+        System.out.println(angka2-angka);
+        System.out.println(angka2*angka);
+        System.out.println(angka2/angka);
+        System.out.println(angka2%angka);
+        
+       
+    }
+    
+}
