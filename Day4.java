7a51160dc9f4c7cf6e36d263f56ddb8a6f940409
febriekaa <febryekhawahyuni95@gmail.com>
2023-10-12 20:34:51 +0800

    Day 4

---
+++ Day4.java
@@ -0,0 +1,15 @@
+
+package day.pkg4;
+public class Day4 {
+
+   
+    public static void main(String[] args) {
+      int angka = 20;
+      int nilai= 8;
+        System.out.println(angka<nilai);
+        System.out.println(angka>nilai);
+        System.out.println(nilai!=angka);
+        
+    }
+    
+}
