41a4864f814366a6b7d363117243122b9664d483
febriekaa <febryekhawahyuni95@gmail.com>
2023-10-16 20:41:10 +0800

    Day 8

---
+++ Day8.java
@@ -0,0 +1,14 @@
+package day.pkg8;
+public class Day8 {
+    public static void main(String[] args) {
+     short angka = 702;
+     
+     if(angka%2==0){
+            System.out.println("angka " +angka +" adalah genap");
+     }else {
+            System.out.println("angka" +angka +"adalah ganjil");
+     }
+     
+    }
+    
+}
