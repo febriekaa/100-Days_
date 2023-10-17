3555902018c854386983ed8619ec0f81dcc43a04
febriekaa <febryekhawahyuni95@gmail.com>
2023-10-17 22:47:19 +0800

    Day 9

---
+++ Day9.java
@@ -0,0 +1,37 @@
+package day.pkg9;
+public class Day9 {
+    public static void main(String[] args) {
+       int angka, nilai;
+       angka = 7;
+       nilai= 2;
+       boolean x;
+       
+        x = nilai > angka;
+        System.out.println("apakah nilai lebih besar dari angka: " +x);
+        
+        x = nilai < angka;
+        System.out.println("apakah nilai lebih kecil dari angka: " +x);
+       
+        x = nilai >= angka;
+        System.out.println("apakah nilai lebih besar sama dengan angka:" +x);
+        
+        x= nilai <= angka;
+        System.out.println("apakah nilai lebih kecil sama dengan angka:" +x);
+        
+        x = nilai==angka;
+        System.out.println("apakah nilai sama dengan angka: " +x);
+        
+        x = nilai != angka;
+        System.out.println("apakah nilai tidak sama dengan angka: " +x);
+        
+        
+        
+    }
+}
+
+    
+
+
+    
+    
+
