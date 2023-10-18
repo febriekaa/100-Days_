0714b72abd73290feb2a43e6ba34d80c9c4e2990
febriekaa <febryekhawahyuni95@gmail.com>
2023-10-18 21:08:54 +0800

    Day 10

---
+++ Day10.java
@@ -0,0 +1,20 @@
+package day.pkg10;
+public class Day10 {
+
+    public static void main(String[] args) {
+        int nilai1, nilai2, nilai3;
+        nilai1= 85;
+        nilai2= 90;
+        nilai3= 75;
+        
+        int total;
+        total = (nilai1+ nilai2+ nilai3);
+        System.out.println("Total nilai: " +total );
+        
+        double rata_rata;
+        rata_rata= total / 3;
+        System.out.println("Nilai rata-rata: " +rata_rata);
+        
+    }
+    
+}
