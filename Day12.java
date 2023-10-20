8ba09c08893a218e4536b241efb40133006a569d
febriekaa <febryekhawahyuni95@gmail.com>
2023-10-20 23:30:48 +0800

    Day 12

---
+++ Day12.java
@@ -0,0 +1,16 @@
+
+package day.pkg12;
+public class Day12 {
+
+   
+    public static void main(String[] args) {
+       
+      //memunculkan kelipatan 4 dari angka 2-80
+        for( int b= 2; b<=80; b +=4){
+            System.out.println(b);
+        }
+}
+
+    }
+    
+
