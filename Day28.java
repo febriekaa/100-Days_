2ddd7509d55a5c9f81cb463cd6e972cc28ff21fd
febriekaa <febryekhawahyuni95@gmail.com>
2023-11-06 20:21:09 +0800

    Day 28

---
+++ Day28.java
@@ -0,0 +1,30 @@
+package day.pkg28;
+
+public class Day28 {
+
+    
+    public static void main(String[] args) {
+       //short ke double
+        short Myshort = 172;
+        double shortTodouble = Myshort; 
+        System.out.println("Short ke Double: " + shortTodouble);
+        //char ke long
+        char Mychar = 'R';
+        long charTolong = Mychar;
+        System.out.println("Char ke Long: " + charTolong);
+        // int ke float
+        int Myint  = 702;
+        float intTofloat = Myint;
+        System.out.println("Int ke Float: " + intTofloat);
+        //byte ke long
+        byte Mybyte = 127;
+        long byteTolong = Mybyte;
+        System.out.println("Byte ke Long: " + byteTolong);
+        //float ke double
+        float Myfloat = 2222.2f;
+        double floatTodouble = Myfloat;
+        System.out.println("Float ke Double: " + floatTodouble); 
+    }
+}
+ 
+   
