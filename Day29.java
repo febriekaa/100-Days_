60514fb89d6b5755757d10fddb3e15b81bc190d9
febriekaa <febryekhawahyuni95@gmail.com>
2023-11-07 23:02:02 +0800

    Day 29

---
+++ Day29.java
@@ -0,0 +1,13 @@
+
+package day.pkg29;
+
+
+public class Day29 {
+
+    
+    public static void main(String[] args) {
+        for (int i = 90; i >= 40; i--) 
+            System.out.println(i);
+    }
+    
+}
