8ac38e0f9f39d8123d990c007d898d731c0e293f
febriekaa <febryekhawahyuni95@gmail.com>
2023-11-10 19:18:22 +0800

    Day 32

---
+++ Day32.java
@@ -0,0 +1,18 @@
+package day.pkg32;
+
+
+public class Day32 {
+
+    public static void main(String[] args) {
+       
+        // Array string
+        String[] namaBuah = {"Apel", "Jeruk", "Pisang", "Mangga", "Anggur"};
+
+        
+        System.out.println("Buah yang saya sukai:");
+        System.out.println(namaBuah[3]);
+        
+    }
+}
+
+ 
