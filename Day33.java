f60b26f614929928929ea734754adab3331158d5
febriekaa <febryekhawahyuni95@gmail.com>
2023-11-10 19:28:22 +0800

    Day 33

---
+++ Day33.java
@@ -0,0 +1,35 @@
+package day.pkg33;
+
+public class Day33 {
+
+    public static void main(String[] args) {
+
+//       array integer
+        int[] angka = {2, 4, 6, 8, 10};
+
+//        Menampilkan isi array menggunakan loop
+        System.out.println("Array Angka:");
+        for (int i = 0; i < angka.length; i++) {
+            System.out.println(angka[i]);
+        }
+
+      
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
