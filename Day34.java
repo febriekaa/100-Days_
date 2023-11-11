612d60ef5664aa6a10e703589bcabac1750e9672
febriekaa <febryekhawahyuni95@gmail.com>
2023-11-11 18:43:40 +0800

    Day 34

---
+++ Day34.java
@@ -0,0 +1,20 @@
+package day.pkg34;
+
+
+public class Day34 {
+
+ 
+    public static void main(String[] args) {
+      
+//        Deklarasi dan inisialisasi array string
+        String[] namaHari = {"Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu", "Minggu"};
+
+        // Menampilkan isi array menggunakan loop
+        System.out.println("Daftar Hari dalam Seminggu:");
+        for (int h = 0; h < namaHari.length; h++) {
+            System.out.println("Hari ke-" + (h + 1) + ": " + namaHari[h]);
+        }
+    }
+}
+
+   
