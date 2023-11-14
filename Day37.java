9e984cf4b07a4036c99b3720419e10895d9464f9
febriekaa <febryekhawahyuni95@gmail.com>
2023-11-14 23:11:16 +0800

    Day 37

---
+++ Day37.java
@@ -0,0 +1,23 @@
+package day.pkg37;
+
+public class Day37 {
+
+    public static void main(String[] args) {
+
+        String[] namaArray = {"dyo", "pcy", "osh", "jae", "hae"};
+
+        // Menampilkan setiap nama dalam array
+        System.out.println("Nama dalam array:");
+        for (String nama : namaArray) {
+            System.out.println(nama);
+        }
+
+        // Menampilkan panjang (jumlah elemen) dari array
+        System.out.println("Panjang array: " + namaArray.length);
+    
+    
+    
+    }
+    
+ }
+ 
