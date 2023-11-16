0af53b8e2b71b4b5a8f48501de0f0e0a30aeb49d
febriekaa <febryekhawahyuni95@gmail.com>
2023-11-16 22:23:24 +0800

    Day 39

---
+++ Day39.java
@@ -0,0 +1,17 @@
+package day.pkg39;
+
+public class Day39 {
+
+    public static void main(String[] args) {
+         int[] angkaArray = {20, 40, 60, 80, 100};
+
+        // Menampilkan setiap angka dalam array menggunakan while
+        System.out.println("Angka dalam array (while): ");
+        int i = 0;
+        while (i < angkaArray.length) {
+            System.out.println(angkaArray[i]);
+            i++;
+        }
+    }
+    
+}
