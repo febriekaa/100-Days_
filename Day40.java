1b15fd1a25979877667d59171ba6079745bbc16a
febriekaa <febryekhawahyuni95@gmail.com>
2023-11-17 12:35:53 +0800

    Day 40

---
+++ Day40.java
@@ -0,0 +1,14 @@
+package day.pkg40;
+
+public class Day40 {
+
+    public static void main(String[] args) {
+       int angka = 2;
+        
+        while (angka <= 100){
+            System.out.println(angka);
+            angka += 2;
+        }
+    }
+    
+}
