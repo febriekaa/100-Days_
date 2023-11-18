56420566551ce7d575851e156476e0b9de75ffd0
febriekaa <febryekhawahyuni95@gmail.com>
2023-11-18 22:21:26 +0800

    Day 41

---
+++ Day41.java
@@ -0,0 +1,18 @@
+package day.pkg41;
+
+import java.util.Scanner;
+
+public class Day41 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Masukkan angka: ");
+        int angka = sc.nextInt();
+        
+        for (int i = angka; i >= 0; i--) {
+            System.out.println(i);
+            
+            
+        }
+    }
+    
+}
