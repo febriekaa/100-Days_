2d4964bd8a6656ddbdbbee6f754e58bcdc792cac
febriekaa <febryekhawahyuni95@gmail.com>
2023-11-23 21:26:10 +0800

    Break

---
+++ Day46.java
@@ -0,0 +1,20 @@
+package day.pkg46;
+
+public class Day46 {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 7; i++) {
+            if (i == 5){
+                break;
+            }else{
+                System.out.print(i);
+                        
+                        }
+            }
+            
+        }
+            
+    }
+    
+    
+
