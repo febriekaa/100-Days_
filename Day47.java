d0a5ae952221081345e48625422a5c50c1899941
febriekaa <febryekhawahyuni95@gmail.com>
2023-11-24 20:59:47 +0800

    Break Continue

---
+++ Day47.java
@@ -0,0 +1,15 @@
+package day.pkg47;
+
+public class Day47 {
+    public static void main(String[] args) {
+         for (int i = 0; i <= 10; i++) {
+            if (i == 7){
+                continue;
+            }else{
+                System.out.print(i);
+            }
+            
+        }
+    }
+    
+}
