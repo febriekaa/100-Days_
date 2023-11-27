9c9a25a7aec23c2455826df964950c9089caad44
febriekaa <febryekhawahyuni95@gmail.com>
2023-11-27 21:18:20 +0800

    Pola segitiga

---
+++ Day50.java
@@ -0,0 +1,16 @@
+package day.pkg50;
+
+public class Day50 {
+
+    public static void main(String[] args) {
+        for (int i = 1; i <= 6; i++) {
+            for (int j = 1; j <= i ; j++) {
+                System.out.print("*");
+                
+            }
+            System.out.println();
+            
+        }
+    }
+    
+}
