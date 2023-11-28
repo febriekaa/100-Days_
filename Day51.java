c1f85f8b1f62aba00f830199614c668a6e40eedf
febriekaa <febryekhawahyuni95@gmail.com>
2023-11-28 22:53:54 +0800

    Array menggunakan for

---
+++ Day51.java
@@ -0,0 +1,13 @@
+package day.pkg51;
+public class Day51 {
+
+    public static void main(String[] args) {
+      int[] nilai = {12, 27,19,38,24,62};
+      
+        for (int i = 0; i < nilai.length; i++){
+            System.out.println(nilai[i]);
+            
+        }
+    }
+    
+}
