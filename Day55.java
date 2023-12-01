1969225a2e7140c6554c3ab3c9ed8f753c0efe57
febriekaa <febryekhawahyuni95@gmail.com>
2023-12-01 22:31:52 +0800

    Operator ternany

---
+++ Day55.java
@@ -0,0 +1,17 @@
+package day.pkg55;
+
+public class Day55 {
+
+    public static void main(String[] args) {
+        int angka = 10;
+        
+//        penggunaan operator ternary
+        String hasil = (angka % 2 == 0) ? "Genap" : "Ganjil";
+        
+        
+        System.out.println("Angka ini adalah " + hasil);
+    
+
+    }
+    
+}
