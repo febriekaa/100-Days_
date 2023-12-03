ff43b95a429d6f679fbc768e9b4b1d6f2433311a
febriekaa <febryekhawahyuni95@gmail.com>
2023-12-03 21:02:08 +0800

    Cetak isi Array menggunakan while

---
+++ Day57.java
@@ -0,0 +1,15 @@
+package day.pkg57;
+
+public class Day57 {
+
+    public static void main(String[] args) {
+       int[] angka = {1,2,4,6};
+       
+       int i = 0;
+       while (i < angka.length){
+           System.out.println(angka[i]);
+           i++;
+       }
+    }
+    
+}
