d05694643eb3636afdf66855d106029bb4ec26d6
febriekaa <febryekhawahyuni95@gmail.com>
2023-12-04 23:37:03 +0800

    Cetak isi array menggunakan do-while

---
+++ DAY58.java
@@ -0,0 +1,14 @@
+package day.pkg58;
+public class DAY58 {
+
+    public static void main(String[] args) {
+        int [] nilai = {12,22,42,88};
+       
+       int i = 0;
+       do {
+           System.out.println(nilai[i]);
+           i++;
+       } while (i<nilai.length);
+    }
+    
+}
