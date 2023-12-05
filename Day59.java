32065a6bab5d6b0f34d5748fae2824cc648ce542
febriekaa <febryekhawahyuni95@gmail.com>
2023-12-05 23:02:39 +0800

    Array 2 dimensi

---
+++ Day59.java
@@ -0,0 +1,15 @@
+package day.pkg59;
+
+public class Day59 {
+
+    public static void main(String[] args) {
+         int[][] array = {
+        {2,4,7},
+        {1,5,9},
+        {6,3,8} 
+    };
+        System.out.println(array[2][1]);
+        System.out.println(array[9][4]);
+    }
+    
+}
