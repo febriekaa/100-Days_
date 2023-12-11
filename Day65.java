7c765c36a754405a6268b2a495d4dd980a8b683e
febriekaa <febryekhawahyuni95@gmail.com>
2023-12-11 23:28:12 +0800

    Array 1d menggunakan for each

---
+++ Day65.java
@@ -0,0 +1,13 @@
+package day.pkg65;
+
+public class Day65 {
+
+    public static void main(String[] args) {
+         double[] array = {22.8 ,52.5,98.7,76.4,67.9};
+
+        for(double b : array){
+            System.out.println(b);
+        }
+    }
+    
+}
