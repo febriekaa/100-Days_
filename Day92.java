9ec2ec91fa6ec6194f713ca235b84e17bba9d64e
febriekaa <febryekhawahyuni95@gmail.com>
2024-01-07 18:20:01 +0800

    Mengubah isi array

---
+++ Day92.java
@@ -0,0 +1,15 @@
+package day.pkg92;
+
+import java.util.Arrays;
+
+public class Day92 {
+
+    public static void main(String[] args) {
+        String warna [] = {"biru","ungu","cream"};
+
+        warna [1] = "pink";
+
+        System.out.println(Arrays.toString(warna));
+    }
+    
+}
