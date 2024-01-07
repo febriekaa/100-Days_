96668fcbb3d86326e279417de8ab67f4bee57967
febriekaa <febryekhawahyuni95@gmail.com>
2024-01-07 18:16:58 +0800

    Day 91

---
+++ Day91.java
@@ -0,0 +1,17 @@
+package day.pkg91;
+
+public class Day91 {
+
+    public static void main(String[] args) {
+         int []nilai={2, 4, -15, 72, 80, -3, 8};
+         int count=0;
+        System.out.println("nilai negatif dalam array : ");
+        for (int i = 0; i <nilai.length; i++) {
+            if(nilai[i]<0){
+                System.out.println(nilai[i]);
+            }
+
+    }
+    }
+    
+}
