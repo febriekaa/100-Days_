003152882299727127ec4fe0c69938bf6e0b514e
febriekaa <febryekhawahyuni95@gmail.com>
2024-01-10 23:31:40 +0800

    Day 94

---
+++ Day94.java
@@ -0,0 +1,11 @@
+package day.pkg94;
+public class Day94 {
+    public static void main(String[] args) {
+      String hewan [] = {"Singa","Buaya","Beruang","Macan tutul"};
+
+        for (int i = hewan.length-1; i >= 0; i--) {
+            System.out.println("indeks ke " + i + " = " + hewan[i]); 
+        }
+    }
+    
+}
